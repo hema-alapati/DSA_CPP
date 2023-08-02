46db88db97be1d252c9e4c5840c6444246413ded
Hemalatha <hemalatha2744@gmail.com>
2023-08-02 22:05:33 +0530

    updated

--- 1. Recursion/2print1toN.cpp
+++ 1. Recursion/2print1toN.cpp
@@ -18,3 +18,26 @@ int main()
     printNumber(1, n);
     return 0;
 }
+
+// print linearly without using +
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printLinear(int i, int n)
+{
+    if (i < 1)
+    {
+        return;
+    }
+    printLinear(i - 1, n);
+    cout << i << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printLinear(n, n);
+    return 0;
+}
