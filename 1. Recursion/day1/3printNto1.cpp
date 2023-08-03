62fab42b472a49b96dd6f486852d9fc2e9c23ac0
Hemalatha <hemalatha2744@gmail.com>
2023-08-03 23:51:39 +0530

    added files

---
+++ 1. Recursion/day1/3printNto1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNumber(int i, int n)
+{
+    if (n < i)
+    {
+        return;
+    }
+    cout << n << endl;
+    printNumber(i, n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printNumber(1, n);
+    return 0;
+}
+
+// print n to 1 without -
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printnto1(int i, int n)
+{
+    if (i > n)
+    {
+        return;
+    }
+    printnto1(i + 1, n);
+    cout << i << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printnto1(1, n);
+    return 0;
+}
