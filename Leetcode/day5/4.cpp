aab23ca2e7bb62f53ccad70cfba35556f6af2464
Hemalatha <hemalatha2744@gmail.com>
2023-08-09 11:05:24 +0530

    4

---
+++ Leetcode/day5/4.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int nonZeroIndex = 0; // Pointer to the current position to place the next non-zero element
+
+        // Iterate through the array
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[nonZeroIndex] = nums[i]; // Move non-zero element to the correct position
+                nonZeroIndex++;
+            }
+        }
+
+        // Fill the remaining positions with zeroes
+        while (nonZeroIndex < nums.size())
+        {
+            nums[nonZeroIndex] = 0;
+            nonZeroIndex++;
+        }
+    }
+};
