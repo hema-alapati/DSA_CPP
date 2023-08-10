6d93e17450a3a2b6da69e14d98dec4e4ae31b941
Hemalatha <hemachowdary2744@gmail.com>
2023-08-10 23:04:18 +0530

    updated reverseLL

--- 2.LinkedList/day6/1reverseLinkedList.cpp
+++ 2.LinkedList/day6/1reverseLinkedList.cpp
@@ -27,3 +27,17 @@ public:
         return newHead;
     }
 };
+
+// just the code
+ListNode *reverseList(ListNode *head)
+{
+    ListNode *newHead = NULL;
+    while (head != NULL)
+    {
+        ListNode *next = head->next;
+        head->next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+}
