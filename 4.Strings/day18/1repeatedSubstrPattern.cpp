ba9224053e2fec6a55fa5f8cb193a0f301b29042
Hemalatha <hemachowdary2744@gmail.com>
2023-08-24 13:07:34 +0530

    file1

---
+++ 4.Strings/day18/1repeatedSubstrPattern.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/repeated-substring-pattern/submissions/
+
+class Solution
+{
+public:
+    bool repeatedSubstringPattern(string s)
+    {
+        int n = s.size();
+        for (int i = n / 2; i >= 1; i--)
+        {
+            if (n % i == 0)
+            {
+                if (s.substr(0, n - i) == s.substr(i))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
