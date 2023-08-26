f20a30acd990a8c91b464cf7fd42630d7230c6fd
Hemalatha <hemachowdary2744@gmail.com>
2023-08-26 15:01:04 +0530

    count negative in sorted matrix

---
+++ 3.Arrays/day20/2countNegative.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/submissions/detail/1032124238/
+
+class Solution
+{
+public:
+    int countNegatives(vector<vector<int>> &grid)
+    {
+        int count = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j] < 0)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
