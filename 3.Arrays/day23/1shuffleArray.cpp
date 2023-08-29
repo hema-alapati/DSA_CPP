99f262d16228604b45b7760e10ca27eed1598ec9
Hemalatha <hemachowdary2744@gmail.com>
2023-08-30 00:21:08 +0530

    1

---
+++ 3.Arrays/day23/1shuffleArray.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    vector<int> shuffle(vector<int> &nums, int n)
+    {
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[n + i]);
+        }
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
+//     vector<int> shuffle(vector<int>& nums, int n) {
+//         vector<int>ans;
+//         int j=n;
+//         for(int i=0;i<n;i++){
+//             ans.push_back(nums[i]);
+//             ans.push_back(nums[j++]);
+//         }
+//         return ans;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> shuffle(vector<int>& nums, int n) {
+//         vector<int>ans;
+//         int i=0,j=n;
+//         while((i<n)&&(j<2*n)){
+//             ans.push_back(nums[i]);
+//             ans.push_back(nums[j]);
+//             i++,j++;
+//         }
+//         return ans;
+//     }
+// };
