4958c4b5b076f1f459beebb4a9d454e98536aac9
Hemalatha <hemachowdary2744@gmail.com>
2023-09-04 18:49:25 +0530

    2-2ways

--- 7.Punishment10/04-09-23/2stackUsingQueue.cpp
+++ 7.Punishment10/04-09-23/2stackUsingQueue.cpp
@@ -1,49 +1,91 @@
-#include <bits/stdc++.h>
+// https://www.youtube.com/watch?v=jDZQKzEtbYQ
 
-using namespace std;
-
-class Stack
+// using 2 queues
+class MyStack
 {
-    queue<int> q;
-
 public:
-    void Push(int x)
+    /** Initialize your data structure here. */
+    queue<int> q1;
+    queue<int> q2;
+    MyStack()
     {
-        int s = q.size();
-        q.push(x);
-        for (int i = 0; i < s; i++)
-        {
+    }
 
+    /** Push element x onto stack. */
+    void push(int x)
+    {
+        q2.push(x);
+        while (!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    int pop()
+    {
+        int result = top();
+        q1.pop();
+        return result;
+    }
+
+    /** Get the top element. */
+    int top()
+    {
+        return q1.front();
+    }
+
+    /** Returns whether the stack is empty. */
+    bool empty()
+    {
+        return q1.empty();
+    }
+};
+// optimal, only 1 queue
+class MyStack
+{
+public:
+    queue<int> q;
+    MyStack()
+    {
+        ;
+    }
+
+    void push(int x)
+    {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++)
+        {
             q.push(q.front());
             q.pop();
         }
     }
-    int Pop()
-    {
-        int n = q.front();
-        q.pop();
-        return n;
-    }
-    int Top()
+
+    int top()
     {
         return q.front();
     }
-    int Size()
+
+    int pop()
     {
-        return q.size();
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    bool empty()
+    {
+        return q.empty();
     }
 };
 
-int main()
-{
-    Stack s;
-    s.Push(3);
-    s.Push(2);
-    s.Push(4);
-    s.Push(1);
-    cout << "Top of the stack: " << s.Top() << endl;
-    cout << "Size of the stack before removing element: " << s.Size() << endl;
-    cout << "The deleted element is: " << s.Pop() << endl;
-    cout << "Top of the stack after removing element: " << s.Top() << endl;
-    cout << "Size of the stack after removing element: " << s.Size();
-}
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
