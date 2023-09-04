1a3c1dfc0553accb8268daa5d408b8458f7a5452
Hemalatha <hemachowdary2744@gmail.com>
2023-09-04 21:42:06 +0530

    4,5

---
+++ 7.Punishment10/04-09-23/4validParenthesis.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;  //taking stack for keep tracking the order of the brackets..
+        for(auto i:s)  //iterate over each and every elements
+        {
+            if(i=='(' or i=='{' or i=='[') st.push(i);  //push to stack if it is an open bracket
+            else  //if it is a closing bracket, check two conditions  current element matches with top of the stack and the stack must not be empty
+            {
+                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
+                st.pop();  //if control reaches to this line, it means we have got the right pair of brackets, so just pop it.
+            }
+        }
+        return st.empty();  //at last, it may possible that we left something into the stack unpair so return checking stack is empty or not..
+    }
+};
