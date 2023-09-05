3a0d68b39c8945a9fde1b5877bfceaefa7c9871f
Hemalatha <hemachowdary2744@gmail.com>
2023-09-05 13:32:02 +0530

    4, medium problem

---
+++ 8. Binary Tree/day30/4maxWidth.cpp
@@ -0,0 +1,79 @@
+// https://leetcode.com/problems/maximum-width-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// run time error due to overflow
+// class Solution {
+// public:
+//     int widthOfBinaryTree(TreeNode* root) {
+//         if(!root){
+//             return 0;
+//         }
+//         int ans=0;
+//         queue<pair<TreeNode*, int>>q;
+//         q.push({root,0});
+//         while(!q.empty()){
+//             int size=q.size();
+//             int mmin=q.front().second; //to make the id start from 0
+//             int first, last;
+//             for(int i=0;i<size;i++){
+//                 int cur_id=q.front().second-mmin;
+//                 TreeNode* node=q.front().first;
+//                 q.pop();
+//                 if(i==0) first=cur_id;
+//                 if(i==size-1) last=cur_id;
+//                 if(node->left) q.push({node->left,cur_id*2+1});
+//                 if(node->right) q.push({node->right, cur_id*2+2});
+//             }
+//             ans=max(ans,last-first+1);
+//         }
+//         return ans;
+//     }
+// };
+
+// using long long
+class Solution
+{
+public:
+    int widthOfBinaryTree(TreeNode *root)
+    {
+        if (!root)
+        {
+            return 0;
+        }
+        int ans = 0;
+        queue<pair<TreeNode *, long long>> q; // Use long long for indices
+        q.push({root, 0});
+        while (!q.empty())
+        {
+            int size = q.size();
+            long long mmin = q.front().second; // Use long long for mmin
+            long long first, last;
+            for (int i = 0; i < size; i++)
+            {
+                long long cur_id = q.front().second - mmin;
+                TreeNode *node = q.front().first;
+                q.pop();
+                if (i == 0)
+                    first = cur_id;
+                if (i == size - 1)
+                    last = cur_id;
+                if (node->left)
+                    q.push({node->left, cur_id * 2 + 1});
+                if (node->right)
+                    q.push({node->right, cur_id * 2 + 2});
+            }
+            ans = max(ans, static_cast<int>(last - first + 1)); // Cast to int for ans
+        }
+        return ans;
+    }
+};
