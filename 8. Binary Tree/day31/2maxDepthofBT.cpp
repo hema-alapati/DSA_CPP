e7616629466b9ebcf926008036335cf37b64d088
Hemalatha <hemachowdary2744@gmail.com>
2023-09-06 15:53:45 +0530

    easy

---
+++ 8. Binary Tree/day31/2maxDepthofBT.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/submissions/detail/1042001868/
+
+//recursive solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+        int lh=maxDepth(root->left);
+        int rh=maxDepth(root->right);
+        return 1+max(lh,rh);
+    }
+};
