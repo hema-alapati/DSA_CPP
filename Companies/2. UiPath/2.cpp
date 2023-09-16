d1803e8e8307ae9ccabafc81cafe42b727ebefa9
Hemalatha <hemachowdary2744@gmail.com>
2023-09-16 12:07:49 +0530

    2nd

--- Companies/2. UiPath/2.cpp
+++ Companies/2. UiPath/2.cpp
@@ -31,3 +31,94 @@
 // . 1 ≤ ms 105
 // • 1 < burstTimelils 109
 
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isFeasible(vector<int>& burstTime, int n, int m, int maxLoad) {
+    int currentLoad = 0;
+    int servers = 1;
+
+    for (int i = 0; i < m; ++i) {
+        if (currentLoad + burstTime[i] <= maxLoad) {
+            currentLoad += burstTime[i];
+        } else {
+            servers += 1;
+            currentLoad = burstTime[i];
+        }
+
+        if (servers > n) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+long int getMinMaxLoadTime(int n, vector<int>& burstTime) {
+    long int left = *max_element(burstTime.begin(), burstTime.end());
+    long int right = accumulate(burstTime.begin(), burstTime.end(), 0);
+
+    while (left < right) {
+        long int mid = left + (right - left) / 2;
+        if (isFeasible(burstTime, n, burstTime.size(), mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of resources: ";
+    cin >> n;
+
+    vector<int> burstTime;
+    cout << "Enter the burst times separated by spaces: ";
+    for (int i = 0; i < n; ++i) {
+        int time;
+        cin >> time;
+        burstTime.push_back(time);
+    }
+
+    long int result = getMinMaxLoadTime(n, burstTime);
+    cout << "Minimum max load of the job schedule: " << result << endl;
+
+    return 0;
+}
+
+
+// python
+// def getMinMaxLoadTime(n, burstTime):
+//     def is_feasible(burstTime, n, m, max_load):
+//         current_load = 0
+//         servers = 1
+
+//         for i in range(m):
+//             if current_load + burstTime[i] <= max_load:
+//                 current_load += burstTime[i]
+//             else:
+//                 servers += 1
+//                 current_load = burstTime[i]
+            
+//             if servers > n:
+//                 return False
+        
+//         return True
+
+//     left = max(burstTime)  # Minimum possible load
+//     right = sum(burstTime)  # Maximum possible load
+
+//     while left < right:
+//         mid = (left + right) // 2
+//         if is_feasible(burstTime, n, len(burstTime), mid):
+//             right = mid
+//         else:
+//             left = mid + 1
+
+//     return left
