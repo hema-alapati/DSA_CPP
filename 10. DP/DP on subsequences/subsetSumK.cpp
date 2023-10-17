8f9d74492da6cc8009c1c9b1376150cf133d3047
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-17 14:55:40 +0530

    Update subsetSumK.cpp

--- 10. DP/DP on subsequences/subsetSumK.cpp
+++ 10. DP/DP on subsequences/subsetSumK.cpp
@@ -18,7 +18,7 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     return f(n-1,k,arr);
 }
 
--------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
 
 #include <bits/stdc++.h> 
 //Memoization
@@ -40,3 +40,40 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     vector<vector<int>>dp(n,vector<int>(k+1,-1));
     return f(n-1,k,arr,dp);
 }
+
+// -------------------------------------------------------------------------------------
+// tabulation
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<bool>>dp(n,vector<bool>(k+1,0));
+    for(int i=0;i<n;i++) dp[i][0]=true;
+    dp[0][arr[0]]=true;
+    for(int ind=1;ind<n;ind++){
+        for(int target=1;target<=k;target++){
+            bool notTake=dp[ind-1][target];
+            bool take=false;
+            if(arr[ind]<=target) take=dp[ind-1][target-arr[ind]];
+            dp[ind][target]=take|notTake;
+        }
+    }
+    return dp[n-1][k];
+}
+
+// -------------------------------------------------------------------------------------------
+// space optimization
+    bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<bool>prev(k+1,0),cur(k+1,0);
+    prev[0]=cur[0]=true;
+    prev[arr[0]]=true;
+    for(int ind=1;ind<n;ind++){
+        for(int target=1;target<=k;target++){
+            bool notTake=prev[target];
+            bool take=false;
+            if(arr[ind]<=target) take=prev[target-arr[ind]];
+            cur[target]=take|notTake;
+        }
+        prev=cur;
+    }
+    return prev[k];
+}
+
+// -------------------------------------------------------------------------------------------
