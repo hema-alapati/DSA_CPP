0f2ea9b1eeea9a856f4e84958b4449ac35333e1a
hema-alapati <95674870+hema-alapati@users.noreply.github.com>
2023-10-17 13:17:06 +0530

    Update subsetSumK.cpp
    recursion

--- 10. DP/DP on subsequences/subsetSumK.cpp
+++ 10. DP/DP on subsequences/subsetSumK.cpp
@@ -1,2 +1,19 @@
 // https://www.codingninjas.com/studio/problems/subset-sum-equal-to-k_1550954?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
 
+#include <bits/stdc++.h> 
+
+bool f(int ind, int target, vector<int>&arr){
+    if(target==0) return true;
+    if(ind==0) return(arr[0]==target);
+
+    bool notTake=f(ind-1,target,arr);
+    bool take=false;
+    if(arr[ind]<=target) take=f(ind-1,target-arr[ind], arr);
+
+    return take|notTake;
+
+}
+
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    return f(n-1,k,arr);
+}
